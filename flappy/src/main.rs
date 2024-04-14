dfbc1e3eee1d9f23c4ecca68dec5cf0959bc6436
Bernardo F Pereira <bernardofpereira@live.com>
2024-04-14 19:30:40 +0000

    added main menu -- not fully functional

--- flappy/src/main.rs
+++ flappy/src/main.rs
@@ -1,10 +1,50 @@
 use bracket_lib::prelude::*;
 
-struct State {}
+enum GameMode {
+    Menu,
+    Playing,
+    End,
+}
+
+struct State {
+    mode: GameMode,
+}
+impl State {
+    fn new() -> Self {
+        Self {
+            mode: GameMode::Menu,
+        }
+    }
+
+    fn play(&mut self, ctx: &mut BTerm) {
+        // TODO: fill in later
+        self.mode = GameMode::End;
+    }
+    fn restart(&mut self) {
+        self.mode = GameMode::Playing;
+    }
+    fn main_menu(&mut self, ctx: &mut BTerm) {
+        ctx.cls();
+        ctx.print_centered(5, "Welcome to Flappy Dragon");
+        ctx.print_centered(8, "[P]lay Game");
+        ctx.print_centered(9, "[Q]uit");
+
+        if let Some(key) = ctx.key {
+            match key {
+                VirtualKeyCode::P => self.restart(),
+                VirtualKeyCode::Q => ctx.quitting = true,
+                _ => {}
+            }
+        }
+    }
+}
 impl GameState for State {
     fn tick(&mut self, ctx: &mut BTerm) {
-        ctx.cls();
-        ctx.print(1, 1, "Hello bracket!");
+        match self.mode {
+            GameMode::Menu => self.main_menu(ctx),
+            GameMode::End => self.dead(ctx),
+            GameMode::Playing => self.play(ctx),
+        }
     }
 }
 
@@ -13,7 +53,7 @@ fn main() -> BError {
         .with_title("Flappy Tutorial")
         .build()?;
 
-    let gs = State {};
+    let gs = State::new();
 
     main_loop(context, gs)
 }

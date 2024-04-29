48420b58ad40b93e9dcdc3f108d8244501735446
Bernardo F Pereira <bernardofpereira@live.com>
2024-04-29 21:33:25 +0000

    aplified tiles + added camera

--- dungeon-crawl/src/main.rs
+++ dungeon-crawl/src/main.rs
@@ -1,3 +1,4 @@
+mod camera;
 mod map;
 mod map_builder;
 mod player;
@@ -6,6 +7,9 @@ mod prelude {
     pub use bracket_lib::prelude::*;
     pub const SCREEN_WIDTH: i32 = 80;
     pub const SCREEN_HEIGHT: i32 = 50;
+    pub const DISPLAY_WIDTH: i32 = SCREEN_WIDTH / 2;
+    pub const DISPLAY_HEIGHT: i32 = SCREEN_HEIGHT / 2;
+    pub use crate::camera::*;
     pub use crate::map::*;
     pub use crate::map_builder::*;
     pub use crate::player::*;
@@ -16,6 +20,7 @@ use prelude::*;
 struct State {
     map: Map,
     player: Player,
+    camera: Camera,
 }
 impl State {
     fn new() -> Self {
@@ -24,23 +29,32 @@ impl State {
         Self {
             map: map_builder.map,
             player: Player::new(map_builder.player_start),
+            camera: Camera::new(map_builder.player_start),
         }
     }
 }
 impl GameState for State {
     fn tick(&mut self, ctx: &mut BTerm) {
+        ctx.set_active_console(0);
         ctx.cls();
-        self.player.update(ctx, &self.map);
-        self.map.render(ctx);
-        self.player.render(ctx);
+        ctx.set_active_console(1);
+        ctx.cls();
+        self.player.update(ctx, &self.map, &mut self.camera);
+        self.map.render(ctx, &self.camera);
+        self.player.render(ctx, &self.camera);
     }
 }
 
 fn main() -> BError {
     let gs = State::new();
-    let ctx = BTermBuilder::simple80x50()
+    let ctx = BTermBuilder::new()
         .with_title("Crusty Rogue")
         .with_fps_cap(30.0)
+        .with_dimensions(DISPLAY_WIDTH, DISPLAY_HEIGHT)
+        .with_font("terminal8x8.png", 8, 8)
+        .with_tile_dimensions(32, 32)
+        .with_simple_console(DISPLAY_WIDTH, DISPLAY_HEIGHT, "terminal8x8.png")
+        .with_simple_console_no_bg(DISPLAY_WIDTH, DISPLAY_HEIGHT, "terminal8x8.png")
         .build()?;
 
     main_loop(ctx, gs)
--- dungeon-crawl/src/map_builder.rs
+++ dungeon-crawl/src/map_builder.rs
@@ -28,8 +28,8 @@ impl MapBuilder {
             let room = Rect::with_size(
                 rng.range(1, SCREEN_WIDTH - 10),
                 rng.range(1, SCREEN_HEIGHT - 10),
-                rng.range(4, 10),
-                rng.range(4, 10),
+                rng.range(2, 10),
+                rng.range(2, 10),
             );
             let mut overlap = false;
             for r in self.rooms.iter() {

15fbc8410921aa4e55205f891fb919b96f8741d0
Bernardo F Pereira <bernardofpereira@live.com>
2024-05-04 01:25:28 +0000

    Adapted project to use Legion ECS

---
+++ dungeon-crawl/src/systems/player_input.rs
@@ -0,0 +1,35 @@
+use crate::prelude::*;
+
+#[system]
+#[write_component(Point)]
+#[read_component(Player)]
+pub fn player_input(
+    ecs: &mut SubWorld,
+    #[resource] map: &Map,
+    #[resource] key: &Option<VirtualKeyCode>,
+    #[resource] camera: &mut Camera,
+) {
+    if let Some(key) = key {
+        let delta = match key {
+            VirtualKeyCode::Numpad8 => Point::new(0, -1),
+            VirtualKeyCode::Numpad2 => Point::new(0, 1),
+            VirtualKeyCode::Numpad6 => Point::new(1, 0),
+            VirtualKeyCode::Numpad4 => Point::new(-1, 0),
+            VirtualKeyCode::Numpad7 => Point::new(-1, -1),
+            VirtualKeyCode::Numpad9 => Point::new(1, -1),
+            VirtualKeyCode::Numpad1 => Point::new(-1, 1),
+            VirtualKeyCode::Numpad3 => Point::new(1, 1),
+            _ => Point::new(0, 0),
+        };
+        if delta.x != 0 || delta.y != 0 {
+            let mut players = <&mut Point>::query().filter(component::<Player>());
+            players.iter_mut(ecs).for_each(|pos| {
+                let destination = *pos + delta;
+                if map.can_enter_tile(destination) {
+                    *pos = destination;
+                    camera.on_player_move(destination);
+                }
+            });
+        }
+    }
+}
